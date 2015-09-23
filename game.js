34a743478728a3f444472f1cd455b0114e34a3ff
Douglas Smith <doug.smith34324@gmail.com>
2015-09-23 10:06:16 -0500

    general clean up + added nice looking number roll

--- game.js
+++ game.js
@@ -3,6 +3,34 @@
 * Program created by Douglas Smith.
 */
 
+/* Misc functions */
+/* Rolling number animation (looks nicer) */
+function rollNumbers(id, start, end, duration) {
+	var obj = document.getElementById(id);
+    var range = end - start;
+    var minTimer = 50;
+
+    var stepTime = Math.abs(Math.floor(duration/range));
+    stepTime = Math.max(stepTime, minTimer);
+
+    var startTime = new Date().getTime();
+    var endTime = startTime + duration;
+    var timer;
+
+    function run() {
+        var now = new Date().getTime();
+        var remaining = Math.max((endTime - now) / duration, 0);
+        var value = Math.round(end - (remaining * range));
+        obj.innerHTML = value + " clicks";
+        if (value == end) {
+            clearInterval(timer);
+        }
+    }
+
+    var timer = setInterval(run, stepTime);
+    run();
+}
+
 Game = {};
 
 Game.Init = function() {
@@ -10,6 +38,7 @@ Game.Init = function() {
 	var clicks = 0;
 	var cps = 0.10;
 	var clickPower = 1;
+	var start = 0;
 	
 	var firstCost = 10;
 	var secondCost = 100;
@@ -20,15 +49,17 @@ Game.Init = function() {
 
 	
 	Game.increment = function() {
+		start = clicks;
 		clicks = clicks + clickPower;
 		document.getElementById("clicks").value = clicks;
-		document.getElementById("clicks").innerHTML = Math.floor(clicks) + " clicks";
+		rollNumbers("clicks", Math.floor(start), Math.floor(clicks), 100);
 	}
 
 	Game.timedIncrement = function() {
+		start = clicks;
 		clicks = clicks + cps;
-		document.getElementById("clicks").value = clicks;
-		document.getElementById("clicks").innerHTML = Math.floor(clicks) + " clicks";
+		document.getElementById("clicks").value = clicks + cps;
+		rollNumbers("clicks", Math.floor(start), Math.floor(clicks), 300);
 	}
 	
 	/* Check whether buttons should be active based on cost */
@@ -44,13 +75,13 @@ Game.Init = function() {
 	}
 
 	Game.firstButton = function() {
-		clicks = document.getElementById("clicks").value;
+		clicks, start = document.getElementById("clicks").value;
 		if (clicks >= firstCost) {
 			clicks = clicks - firstCost;
 			document.getElementById("clicks").value = clicks;
-			document.getElementById("clicks").innerHTML = Math.floor(clicks) + " clicks";
+			rollNumbers("clicks", Math.floor(start), Math.floor(clicks), 300);
 			
-			cps = cps + 1;
+			cps = cps + 0.5;
 			document.getElementById("cps").value = cps;
 			document.getElementById("cps").innerHTML = Math.round(cps * 100) / 100 + " cps";
 			
@@ -61,11 +92,11 @@ Game.Init = function() {
 	}
 	
 	Game.secondButton = function() {
-		clicks = document.getElementById("clicks").value;
+		clicks, start = document.getElementById("clicks").value;
 		if (clicks >= secondCost) {
 			clicks = clicks - secondCost;
 			document.getElementById("clicks").value = clicks;
-			document.getElementById("clicks").innerHTML = Math.floor(clicks) + " clicks";
+			rollNumbers("clicks", Math.floor(start), Math.floor(clicks), 300);
 			
 			cps = cps + 10;
 			document.getElementById("cps").value = cps;
@@ -77,12 +108,9 @@ Game.Init = function() {
 		}
 	}
 	
-	Game.displayFirstInfo = function() {
-		document.getElementById("firstInfo").style.display = "block";
-	}
-	
-	Game.hideFirstInfo = function() {
-		document.getElementById("firstInfo").style.display = "none";
+	Game.toggleButtonOptions = function() {
+		document.getElementById("firstInfo").style.display = (document.getElementById("firstInfo").style.display != "block" ? "block" : "none");
+		document.getElementById("secondInfo").style.display = (document.getElementById("secondInfo").style.display != "block" ? "block" : "none");
 	}
 	
 	Game.upgradeClicks = function() {
